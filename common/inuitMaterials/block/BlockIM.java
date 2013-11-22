5d963dc97c2675038626e77b7beddcefad36def9
ZL123 <jucha00@gmail.com>
2013-11-22 17:04:29 +0000

    Added a couple more things and moved Materials.txt to the Resources folder

---
+++ common/inuitMaterials/block/BlockIM.java
@@ -0,0 +1,12 @@
+package inuitMaterials.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockIM extends Block {
+
+	public BlockIM(int id, Material material) {
+		super(id, material);
+	}
+
+}
